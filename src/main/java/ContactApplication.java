5a3b79011eaab400592a3afed74de863149c18df
sandeeprawat4 <sandeep.rawat4@gmail.com>
2022-05-30 13:54:12 +0530

    Basic Structure

--- src/main/java/ContactApplication.java
+++ src/main/java/ContactApplication.java
@@ -6,15 +6,15 @@ import java.io.*;
 
 public class ContactApplication {
     public static void main(String[] args){
-        VCard sandeep = new VCard("Sandeep Rawat")
-                .setEmail("sandeep.rawat4@gmail.com")
-                .setAddress("Hyderabad, India")
+        VCard sandeep = new VCard("John Doe")
+                .setEmail("xyz@abc.com")
+                .setAddress("USA")
                 .setTitle("Mister")
-                .setCompany("OpenText")
-                .setPhoneNumber("7799345525")
-                .setWebsite("www.sandeep.com");
+                .setCompany("ABC Corp")
+                .setPhoneNumber("999999999")
+                .setWebsite("www.abc.com");
         ByteArrayOutputStream stream = QRCode.from(sandeep).stream();
-        try(OutputStream outputStream = new FileOutputStream("/users/rawats/desktop/qrCode.jpg")) {
+        try(OutputStream outputStream = new FileOutputStream("/users/qrCode.jpg")) {
             stream.writeTo(outputStream);
         } catch (IOException e) {
             e.printStackTrace();
